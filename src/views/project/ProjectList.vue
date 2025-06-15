1eea6368a091cf190366ff1ded8d18ab2f1e39d4
mins00kwon <mskwon0918@gmail.com>
2025-06-15 16:44:08 +0900

    feat: 프로젝트 목록 조회 구현중 - 목록 조회 자체는 잘 되는중 - 진척률 및 경과율 도넛 차트 구현 예정

--- src/views/project/ProjectList.vue
+++ src/views/project/ProjectList.vue
@@ -3,14 +3,38 @@
 import Breadcrumb from '@/components/common/BreadCrumb.vue';
 import ListLayout from '@/components/layout/ListLayout.vue';
 
+import { ref, onMounted } from 'vue';
+import api from '@/api';
+import ProjectCard from '@/components/project/ProjectCard.vue';
+
+const projectList = ref([])
+onMounted(async () => {
+  try {
+    const res = await api.get('/api/projects/list');
+    projectList.value = res.data.data;
+  } catch(err){
+    console.error('프로젝트 목록 조회 실패', err);
+  }
+})
+
 </script>
 
 <template>
 
 
     <ListLayout title="프로젝트 목록" >
+      <div class="search-bar">
+        <input type="text" placeholder="프로젝트 검색"/>
+        <button>filter</button>
+        <button class="create-btn">+ 프로젝트 생성하기</button>
+      </div>
 
-        프로젝트 목록
+      <div class="project-list">
+        <ProjectCard
+        v-for="project in projectList"
+        :key="project.id"
+        :project="project"/>
+      </div>
     </ListLayout>
 
 
@@ -24,6 +48,22 @@ import ListLayout from '@/components/layout/ListLayout.vue';
   min-height: 600px;
   border-radius: 8px;
   padding: 20px;
+}
+.search-bar {
+  display: flex;
+  gap: 12px;
+  margin-bottom: 20px;
+}
+.project-list {
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+}
+.create-btn {
+  background-color: #00c6a7;
+  color: white;
+  border-radius: 4px;
+  padding: 6px 12px;
 }
 </style>
 
