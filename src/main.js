b8d7ef5334b9fed515c6e66574580830894a2681
Hailyee <a11roundworld@naver.com>
2025-06-16 16:30:38 +0900

    간트구현중간저장

--- src/main.js
+++ src/main.js
@@ -4,12 +4,11 @@ import router from './router/index.js'
 import { createPinia } from 'pinia' 
 import vuetify from './plugins/vuetify'
 import { GanttPlugin } from '@syncfusion/ej2-vue-gantt';
-import '@syncfusion/ej2-base/styles/material.css';
-import '@syncfusion/ej2-vue-gantt/styles/material.css';
+
 import { registerLicense } from '@syncfusion/ej2-base';
 
-// 여기에 라이선스 키 붙여넣기
-registerLicense('Ngo9BigBOggjHTQxAR8/V1NNaF1cWWhPYVF0WmFZfVtgdVdMYF9bR3JPIiBoS35Rc0VlWHpfcnRWRWRVUkd+VEBU');
+// ✅ .env에서 키 불러오기
+registerLicense(import.meta.env.VITE_SYNCFUSION_LICENSE_KEY);
 
 const app = createApp(App)
 
