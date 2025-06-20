00ab31c773c014a6eb54afc91b46d95d3bfe85f4
minsun24 <jms49526@gmail.com>
2025-06-20 09:27:34 +0900

    refactor : 프로젝트 디테일 레이아웃 수정

--- src/views/project/ProjectDetail.vue
+++ src/views/project/ProjectDetail.vue
@@ -1,47 +1,50 @@
 <template>
-  <div class="project-layout">
+  <div class="project-container">
     <!-- 상단 Breadcrumb -->
     <BreadCrumb :items="[
       { text: '프로젝트', to: '/project/list' },
       { text: projectName, to: `/project/${projectId}` }
     ]" />
+    <div class="project-layout">
+      <!-- 페이지 타이틀 -->
+      <h1 class="page-title">
+        📁 {{ projectName }}
+        <ProjectStatusButton
+          :status="projectStatus"
+          :projectInfo="projectInfo"
+          :allTaskList="allTaskList"
+          :isDirector="isDirector"
+          :completedTaskList = "completedTaskList"
+          @start="markAsInProgress"
+          @complete="markAsCompleted"
+          @deleted="markAsDeleted"
+          @report="downloadReport"
+          @restart="markAsRestart"
+          @restore="markAsRestore"
+          @canceled="markAsCanceled"
+        />
+      </h1>
 
-    <!-- 페이지 타이틀 -->
-    <h1 class="page-title">
-      📁 {{ projectName }}
-      <ProjectStatusButton
-        :status="projectStatus"
-        :projectInfo="projectInfo"
-        :allTaskList="allTaskList"
-        :isDirector="isDirector"
-        :completedTaskList = "completedTaskList"
-        @start="markAsInProgress"
-        @complete="markAsCompleted"
-        @deleted="markAsDeleted"
-        @report="downloadReport"
-        @restart="markAsRestart"
-        @restore="markAsRestore"
-        @canceled="markAsCanceled"
-      />
-    </h1>
+      <!-- 탭 메뉴 -->
+      <div class="tab-menu">
+        <router-link
+          v-for="tab in tabs"
+          :key="tab.name"
+          :to="tab.route"
+          class="tab-button"
+          :class="{ active: $route.name === tab.name }"
+        >
+          {{ tab.label }}
+        </router-link>
+      </div>
 
-    <!-- 탭 메뉴 -->
-    <div class="tab-menu">
-      <router-link
-        v-for="tab in tabs"
-        :key="tab.name"
-        :to="tab.route"
-        class="tab-button"
-        :class="{ active: $route.name === tab.name }"
-      >
-        {{ tab.label }}
-      </router-link>
+      <!-- 콘텐츠 영역 -->
+      <div class="content-box" :class="{ 'pipeline-active': $route.name === 'ProjectPipeline' }">
+        <router-view />
+      </div>
     </div>
 
-    <!-- 콘텐츠 영역 -->
-    <div class="content-box" :class="{ 'pipeline-active': $route.name === 'ProjectPipeline' }">
-      <router-view />
-    </div>
+    
   </div>
 </template>
 
