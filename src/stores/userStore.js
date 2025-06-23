6ed90d9381bff2c8cbee8d2ed2380caec0efa16a
swjang7269 <azamiyuko7269@gmail.com>
2025-06-23 11:45:37 +0900

    REFACTOR 로그아웃 조건 수정

--- src/stores/userStore.js
+++ src/stores/userStore.js
@@ -210,7 +210,7 @@ export const useUserStore = defineStore('user', () => {
             schemaName.value = savedSchemaName
             accessToken.value = savedAccessToken
         } else {
-            forceLogout()
+            clearState()
         }
     }
 
