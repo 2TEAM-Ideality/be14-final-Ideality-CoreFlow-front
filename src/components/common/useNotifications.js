18e6f9205b29faabb142c085b941744f7895b870
Hailyee <a11roundworld@naver.com>
2025-06-23 17:41:32 +0900

    알림도 axios로 수정

--- src/components/common/useNotifications.js
+++ src/components/common/useNotifications.js
@@ -50,7 +50,7 @@ export function useNotifications() {
       setTimeout(() => {
         console.info('SSE 재연결 시도...');
         connectToSSE(token);
-      }, 5000);  // 재연결 지연 시간 (5초)
+      }, 6000);  // 재연결 지연 시간 (6초)
     };
   };
   
